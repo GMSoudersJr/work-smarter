56de061f110059564e7363f25e988a36ccedfee1
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2025-02-01 20:54:16 +0900

    feat: add error page boundary

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="error-page">
+	<h1 class="all-caps-800">{page.status}: {page.error?.message.toUpperCase()}</h1>
+	<p class="regular-font">The page you are looking for does not exist</p>
+</div>
+
+<style>
+	.error-page {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		place-self: center;
+	}
+</style>
